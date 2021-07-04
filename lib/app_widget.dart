eba74dbf5ad4319da7421898095d94b3a9bd5d5b
kaio <hello@kaio.dev>
2021-07-04 18:21:52 -0300

    adiciona imagens

--- lib/app_widget.dart
+++ lib/app_widget.dart
@@ -1,10 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:level_up_03/modules/get_started/get_started_page.dart';
-
 import 'modules/create_account/create_account_page.dart';
 import 'modules/login/login_page.dart';
 import 'modules/reset_password/reset_password_page.dart';
-import 'modules/splash/splash_page.dart';
 
 class AppWidget extends StatelessWidget {
   @override
@@ -13,11 +11,10 @@ class AppWidget extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
-        primarySwatch: Colors.lightGreen,
+        primaryColor: Color(0xFF3EBC90),
       ),
-      initialRoute: '/splash',
+      initialRoute: '/get-started',
       routes: {
-        '/splash': (context) => SplashPage(),
         '/get-started': (context) => GetStartedPage(),
         '/login': (context) => LoginPage(),
         '/create-account': (context) => CreateAccountPage(),
--- lib/shared/widgets/button/button_widget.dart
+++ lib/shared/widgets/button/button_widget.dart
@@ -10,17 +10,24 @@ class ButtonWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ElevatedButton(
-      onPressed: onPressed,
-      child: Text(
-        text,
-        style: TextStyle(color: Colors.white),
-      ),
-      style: ElevatedButton.styleFrom(
-        shape: StadiumBorder(),
-        primary: color,
-        elevation: 0,
-        minimumSize: Size(128, 48),
+    return Padding(
+      padding: EdgeInsets.fromLTRB(16, 8, 16, 8),
+      child: ElevatedButton(
+        onPressed: onPressed,
+        child: Text(
+          text,
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 16.0,
+            fontWeight: FontWeight.w500,
+          ),
+        ),
+        style: ElevatedButton.styleFrom(
+          shape: StadiumBorder(),
+          primary: color,
+          elevation: 0,
+          minimumSize: Size(128, 48),
+        ),
       ),
     );
   }
